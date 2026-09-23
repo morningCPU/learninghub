c45845ca6036458a8028b68ce68aa67dbfdefa70
“monday-maker” <1512753709@qq.com>
2026-09-23 16:55:36 +0800

    day2

---
+++ 后端核心/01-Java基础与集合框架/2-IDEA安装配置与Maven工程管理/TeatMaven/src/main/java/com/morning/App.java
@@ -0,0 +1,14 @@
+package com.morning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Hello hello = new Hello("morning");
+        hello.say();
+    }
+}
---
+++ 后端核心/01-Java基础与集合框架/2-IDEA安装配置与Maven工程管理/TeatMaven/src/main/java/com/morning/Hello.java
@@ -0,0 +1,21 @@
+package com.morning;
+
+public class Hello {
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void say(){
+        System.out.println("hello " + name);
+    }
+}
